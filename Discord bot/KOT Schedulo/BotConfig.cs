292f1d2e67940aefefdb7f7d9f973b730e3b398b
Nguyễn Trung Quân <116139754+Quan2808@users.noreply.github.com>
2025-05-25 21:59:15 +0700

    refactor to add Discord bot prj

---
+++ Discord bot/KOT Schedulo/BotConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace KOT_Schedulo;
+
+public class BotConfig
+{
+    public string Token { get; set; } = string.Empty;
+    public string Prefix { get; set; } = "!";
+    public ulong GuildId { get; set; } = 0;
+}
---
+++ Discord bot/KOT Schedulo/Program.cs
@@ -0,0 +1,27 @@
+﻿using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace KOT_Schedulo;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var host = CreateHostBuilder(args).Build();
+        
+        var bot = host.Services.GetRequiredService<DiscordBot>();
+        await bot.StartAsync();
+        
+        await host.RunAsync();
+    }
+
+    private static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureServices((context, services) =>
+            {
+                services.AddSingleton<DiscordSocketClient>();
+                services.AddSingleton<DiscordBot>();
+                services.Configure<BotConfig>(context.Configuration.GetSection("Discord"));
+            });
+}
